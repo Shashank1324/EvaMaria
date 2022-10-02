0197dad25297e6f7dcd8dd720e7d6e8b2a1be630
Shashank1324 <87693399+Shashank1324@users.noreply.github.com>
2022-10-02 10:34:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,8 +37,8 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "shashankls")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Rockers_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'MOVIES_ZILAA')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001675164160'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'kannada_Rockerssz')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', 'True')), False)
 IMDB = is_enabled((environ.get('IMDB', 'False')), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', 'True')), False)
